10283de45e7b9979d89cf24735767fb23228eb6d
Bruno Rodrigues <bruno_151997@hotmail.com>
2023-12-11 18:27:29 -0300

    feat: header mobile

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -10,13 +10,13 @@ function handleScrollWindow() {
     header.setAttribute('style', 'padding: 10px;');
     header.classList.add('bg-body-default');
     header.classList.remove('bg-transparent');
-    // logoHeader.setAttribute('style', 'max-height: 70px;');
+    logoHeader.setAttribute('style', 'max-height: 70px;');
   } else {
     header.removeAttribute('style');
     header.classList.remove('bg-body-default');
-    header.classList.setAttribute('style', 'bg-transparent');
+    header.setAttribute('style', 'bg-transparent');
 
-    // logoHeader.removeAttribute('style');
+    logoHeader.removeAttribute('style');
   }
 }
 
@@ -35,17 +35,18 @@ if (menuSide && btnOpenSide && btnCloseSide && sidebar && overlay) {
     menuSide.setAttribute('data-is-open', 'true');
     sidebar.classList.remove('translate-x-full');
     sidebar.classList.add('translate-x-0');
-    btnCloseSide.setAttribute('style', 'opacity:1 ;');
 
+    btnCloseSide.setAttribute('style', 'opacity:1 ; pointer-events: initial;');
     overlay.setAttribute('style', 'opacity:1; pointer-events: initial;');
   }
 
   function handleCloseMenu() {
     menuSide.removeAttribute('data-is-open');
     sidebar.classList.remove('translate-x-0');
-    btnCloseSide.setAttribute('style', 'opacity: 0;');
     sidebar.classList.add('translate-x-full');
 
+    btnCloseSide.removeAttribute('style');
+    btnCloseSide.setAttribute('style', 'opacity: 0;');
     overlay.setAttribute('style', 'opacity:0; pointer-events: none;');
   }
 
